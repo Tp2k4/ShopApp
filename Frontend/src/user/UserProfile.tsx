aca754d4c5b9014fc9d29ab943a9ee32655f5423
Itsmennguyen <tranduy2615@gmail.com>
2025-05-19 17:26:38 +0700

    ..

--- Frontend/src/user/UserProfile.tsx
+++ Frontend/src/user/UserProfile.tsx
@@ -1,6 +1,6 @@
 import Header from "../shared/components/ui/Header";
-import Box from "../shared/components/ui/Box";
 import UserProfileNavigation from "../shared/components/ui/UserProfileNavigation";
+import ProfileInfo from "../shared/components/form/ProfileInfo";
 
 function UserProfile() {
   return (
@@ -9,11 +9,7 @@ function UserProfile() {
 
       <div className="flex justify-center gap-[var(--medium-gap)] w-[75%] h-full">
         <UserProfileNavigation className="w-[25%] h-[400px]" />
-        <Box
-          className="flex flex-col gap-[var(--medium-gap)] p-[var(--medium-gap)]"
-          width="75%"
-          height="400px"
-        ></Box>
+        <ProfileInfo className="flex flex-col gap-[var(--medium-gap)] w-[75%] h-[400px]" />
       </div>
     </div>
   );
