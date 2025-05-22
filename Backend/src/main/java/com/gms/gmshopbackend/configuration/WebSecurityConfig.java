6656c4c9e067d0fc06c22bf8521e7077ce1d0268
Tho Phan <thophan357@gmail.com>
2025-05-22 18:33:32 +0700

    pull

--- Backend/src/main/java/com/gms/gmshopbackend/configuration/WebSecurityConfig.java
+++ Backend/src/main/java/com/gms/gmshopbackend/configuration/WebSecurityConfig.java
@@ -58,13 +58,13 @@ public class WebSecurityConfig {
                             String.format("%s/user/**", apiPrefix)
                             ).permitAll()
                             .requestMatchers(PUT,
-                                    String.format("%s/orders/**", apiPrefix)).hasAnyRole("EMPLOYEE", "ADMIN")
+                                    String.format("%s/orders/employee/**", apiPrefix)).hasAnyRole("EMPLOYEE", "ADMIN")
                             .requestMatchers(POST,
                                     String.format("%s/orders/**", apiPrefix)).hasRole("USER")
                             .requestMatchers(GET,
                                     String.format("%s/orders/user/**", apiPrefix)).hasAnyRole("USER", "ADMIN")
                             .requestMatchers(DELETE,
-                                    String.format("%s/orders/employee/**", apiPrefix)).hasRole("EMPLOYEE")
+                                    String.format("%s/orders/employee/**", apiPrefix)).hasAnyRole("EMPLOYEE","ADMIN")
                             .requestMatchers(GET,
                                     String.format("%s/orders/**", apiPrefix)).hasRole("ADMIN")
                             .requestMatchers(GET,
